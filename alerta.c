6728fb042169e5195612b79a69aa822cb1b647af
brenobvas <brenobvas2011@gmail.com>
2026-04-05 12:39:31 -0300

    Pronto

---
+++ alerta.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "alerta.h"
+
+void emitir_alerta(int n)
+{
+    for 
+    ( int i= 0; i < n; i++ ){
+        printf("BIP!\n");
+    }
+}
---
+++ alerta.h
@@ -0,0 +1,1 @@
+void emitir_alerta(int n)
